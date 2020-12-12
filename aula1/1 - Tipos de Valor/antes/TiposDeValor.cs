c7753d0168f0c9f9d6a5d2a1e3832a707d5e9359
Emerson Vieira <emevieira@gmail.com>
2020-12-12 16:05:14 -0400

    Atribuindo variavel que copia a idade

--- aula1/1 - Tipos de Valor/antes/TiposDeValor.cs
+++ aula1/1 - Tipos de Valor/antes/TiposDeValor.cs
@@ -13,6 +13,16 @@ namespace certificacao_csharp_roteiro.antes
             int idade;
             idade = 30;
             Console.WriteLine(idade);
+
+            int copiaIdade = idade;
+
+            Console.WriteLine($"idade: {idade}");
+            Console.WriteLine($"copiaIdade: {copiaIdade}");
+
+            idade = 23;
+
+            Console.WriteLine($"idade: {idade}");
+            Console.WriteLine($"copiaIdade: {copiaIdade}");
         }
     }
 }
