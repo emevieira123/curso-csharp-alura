dac48fa23b59498f63faef5e7ad13f31282a0c83
Emerson Vieira <emevieira@gmail.com>
2020-12-14 12:25:40 -0400

    Booleanos

--- aula1/5 - Booleanos/antes/Booleanos.cs
+++ aula1/5 - Booleanos/antes/Booleanos.cs
@@ -10,6 +10,30 @@ namespace certificacao_csharp_roteiro.antes
     {
         public void Executar()
         {
+            //bool possuiSaldo = 1;
+
+            bool possuiSaldo = true;
+
+
+            Console.WriteLine($"possuiSaldo: {possuiSaldo}");
+
+            int dias = DateTime.Now.Day;
+            Console.WriteLine($"dias: {dias}");
+
+            //Atribui o diasPar o valor de uma expressão booleana.
+            bool diasPar = (dias % 2 == 0); //Pega o resto da divisão de dias / 2 e faz a verificação se é par ou impar
+
+            if (diasPar == true)
+            {
+                Console.WriteLine("dias é um número par");
+            }
+            else
+            {
+                Console.WriteLine("dias é um número impar");
+            }
+
+
+
         }
     }
 }
