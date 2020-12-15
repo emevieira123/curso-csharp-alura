c89b7929183e94ad566d5ef34228a4eb9d3b68fc
Emerson Vieira <emevieira@gmail.com>
2020-12-15 13:10:12 -0400

    Utilizando delegates

--- aula2/4 - Delegates/antes/Delegates.cs
+++ aula2/4 - Delegates/antes/Delegates.cs
@@ -14,6 +14,8 @@ namespace certificacao_csharp_roteiro.antes
         }
     }
 
+    delegate double MetodoMultiplicacao(double input);
+
     class Calculadora
     {
         static double Duplicar(double input)
@@ -34,6 +36,27 @@ namespace certificacao_csharp_roteiro.antes
 
             //Executa diretamente o método
             Console.WriteLine($"Triplicar(7.5): {Triplicar(7.5)}");
+
+            MetodoMultiplicacao metodoMultiplicacao = Duplicar;
+            Console.WriteLine(metodoMultiplicacao(7.5));
+
+            metodoMultiplicacao = Triplicar;
+            Console.WriteLine($"Triplicar(7.5): {metodoMultiplicacao(7.5)}");
+
+            //instacia um delegate com um método anônimo
+            MetodoMultiplicacao metodoQuadrado = delegate (double input)
+            {
+                return input * input;
+            };
+
+            double quadrado = metodoQuadrado(5);
+            System.Console.WriteLine("quadrado: {0}", quadrado);
+
+            //instacia um delegado com um expressão lambda
+            MetodoMultiplicacao metodoCubo = input => input * input * input;
+
+            double cubo = metodoCubo(4.375);
+            System.Console.WriteLine("cubo: {0}", cubo);
         }
     }
 }
