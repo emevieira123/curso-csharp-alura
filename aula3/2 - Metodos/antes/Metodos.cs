f0b91f2cd73123b6771c83893c6ee2097a58ae5e
Emerson Vieira <emevieira@gmail.com>
2020-12-15 19:04:53 -0400

    Metodos

--- aula3/2 - Metodos/antes/Metodos.cs
+++ aula3/2 - Metodos/antes/Metodos.cs
@@ -11,6 +11,13 @@ namespace certificacao_csharp_roteiro.antes
         public void Executar()
         {
             Retangulo retangulo = new Retangulo(12, 10);
+            System.Console.WriteLine(retangulo.GetArea());
+
+            Retangulo outroRetangulo = new Retangulo(10, 10);
+            System.Console.WriteLine(retangulo.Semelhante(outroRetangulo.Altura, outroRetangulo.Largura));
+            
+            outroRetangulo = new Retangulo(5, 6);
+            System.Console.WriteLine(Retangulo.Semelhante(retangulo, outroRetangulo));
         }
     }
 
@@ -25,14 +32,20 @@ namespace certificacao_csharp_roteiro.antes
             Largura = largura;
 
             Console.WriteLine($"altura: {altura}, largura: {largura}");
+
+            var area = GetArea();
+            System.Console.WriteLine($"area: {area}");
         }
+
+        //Lista de modificadores
+        //public, internal, protected (+internal), private (+internal)
 
         public double GetArea()
         {
             return Altura * Largura;
         }
 
-        bool Semelhante(double outroRetanguloAltura, double outroRetanguloLargura)
+        internal bool Semelhante(double outroRetanguloAltura, double outroRetanguloLargura)
         {
             return
                 ((Largura / Altura) == /*proporção deste retângulo*/
@@ -40,6 +53,16 @@ namespace certificacao_csharp_roteiro.antes
                 ||
                 ((Altura / Largura) == /*compara a proporção inversa*/
                 (outroRetanguloLargura / outroRetanguloAltura));
+        }
+
+         internal static bool Semelhante(Retangulo retangulo, Retangulo outroRetangulo)
+        {
+            return
+                ((retangulo.Largura / retangulo.Altura) == /*proporção deste retângulo*/
+                (outroRetangulo.Largura / outroRetangulo.Altura)) /*proporção do outro retângulo*/
+                ||
+                ((retangulo.Altura / retangulo.Largura) == /*compara a proporção inversa*/
+                (outroRetangulo.Largura / outroRetangulo.Altura));
         }
     }
 }
