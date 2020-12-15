35957572c1fda92c6ffa70e4629a997d517dd078
Emerson Vieira <emevieira@gmail.com>
2020-12-15 16:08:39 -0400

    Objetos

--- aula2/5 - Objetos/antes/Objetos.cs
+++ aula2/5 - Objetos/antes/Objetos.cs
@@ -12,6 +12,21 @@ namespace certificacao_csharp_roteiro.antes
         {
            int pontuacao = 10;
            Console.WriteLine($"pontuacao: {pontuacao}");
+
+           System.Console.WriteLine("OBJECT COM VALOR PRIMITIVO");
+           object meuObjeto;
+           meuObjeto = pontuacao;
+           System.Console.WriteLine($"meuObjeto: {meuObjeto}");
+           System.Console.WriteLine($"meuObjeto.GetType(): {meuObjeto.GetType()}");
+
+           System.Console.WriteLine();
+           System.Console.WriteLine("OBJECT COM REFERÊNCIA DE OBJETO");
+
+           meuObjeto = new Jogador();
+           Jogador classRef;
+           classRef = (Jogador)meuObjeto; //conversão explícita ou "cast"
+           System.Console.WriteLine($"classRef.Pontuacao: {classRef.Pontuacao}");
+
         }
     }
 
