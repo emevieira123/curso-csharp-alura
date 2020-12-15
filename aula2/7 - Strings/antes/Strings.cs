88624151bb978d7c4c986494dfe591f613209fe6
Emerson Vieira <emevieira@gmail.com>
2020-12-15 16:32:11 -0400

    Strings

--- aula2/7 - Strings/antes/Strings.cs
+++ aula2/7 - Strings/antes/Strings.cs
@@ -12,6 +12,17 @@ namespace certificacao_csharp_roteiro.antes
         {
             string a = "bom dia";
             string b = "b";
+
+            //Adiciona ao conteúdo de "b"
+            b = b + "om dia";
+            System.Console.WriteLine($"a == b: {a == b}");
+            System.Console.WriteLine($"(object)a == (object)b: {(object)a == (object)b}");
+
+            string bomDia = "bom dia";
+            System.Console.WriteLine($"bomDia[4]: {bomDia[4]}");
+
+            var caractere = bomDia[4];
+            System.Console.WriteLine($"caractere.GetType(): {caractere.GetType()}");
         }
     }
 }
