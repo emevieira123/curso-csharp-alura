472443e0fac568e27186830bcf430d559aa33989
Emerson Vieira <emevieira@gmail.com>
2020-12-17 16:08:02 -0400

    Sobrecargas Atualizado

--- aula4/1 - Métodos Sobrecarregados/depois/Sobrecargas.cs
+++ aula4/1 - Métodos Sobrecarregados/depois/Sobrecargas.cs
@@ -11,16 +11,16 @@ namespace certificacao_csharp_roteiro
         public void Executar()
         {
             //VOLUME DO CUBO = lado ^ 3;
-            int lado = 3;
+            //int lado = 3;
 
             //VOLUME DO CILINDRO = altura * PI * raio ^ 2;
-            double raio = 2;
-            int altura = 10;
+            //double raio = 2;
+           // int altura = 10;
 
             //VOLUME DO PRISMA = largura * profundidade * altura
-            long largura = 10;
-            altura = 6;
-            int profundidade = 4;
+            //long largura = 10;
+            //altura = 6;
+            //int profundidade = 4;
         }
     }
 }
