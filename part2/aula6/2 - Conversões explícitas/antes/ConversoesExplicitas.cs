75fd698ebea253143e8bb856e10cc4b3590d58e1
Emerson Vieira <emevieira@gmail.com>
2020-12-18 18:39:03 -0400

    Conversoes explicitas

--- part2/aula6/2 - Conversões explícitas/antes/ConversoesExplicitas.cs
+++ part2/aula6/2 - Conversões explícitas/antes/ConversoesExplicitas.cs
@@ -10,6 +10,16 @@ namespace certificacao_csharp_roteiro.antes
     {
         public void Executar()
         {
+            double divida = 1_234_567_890.123;
+            //long copia = divida;
+
+            double salario = 1_237.89;
+            long copiaSalario = (long)salario;
+            System.Console.WriteLine(copiaSalario);
+
+            Animal animal = new Gato();
+            Gato gato = (Gato)animal; //Cast = conversão explícita
+            System.Console.WriteLine(gato.GetType());
         }
     }
 }
