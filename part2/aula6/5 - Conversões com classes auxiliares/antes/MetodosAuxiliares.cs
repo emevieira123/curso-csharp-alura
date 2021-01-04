4dd87b5331084b2cc568a7e4b78faf789584f637
Emerson Vieira <emevieira@gmail.com>
2021-01-04 19:52:04 -0400

    Metodos de conversoes com Parse e TryParse

--- part2/aula6/5 - Conversões com classes auxiliares/antes/MetodosAuxiliares.cs
+++ part2/aula6/5 - Conversões com classes auxiliares/antes/MetodosAuxiliares.cs
@@ -11,6 +11,31 @@ namespace certificacao_csharp_roteiro.antes
     {
         public void Executar()
         {
+            string textoDigitado = "123";
+            // int numeroConvertido = textoDigitado;
+
+            int numeroConvertido = int.Parse(textoDigitado);
+            System.Console.WriteLine(numeroConvertido);
+
+            textoDigitado = "abc";
+            // numeroConvertido = int.Parse(textoDigitado);
+            int.TryParse(textoDigitado, out numeroConvertido); // out torna a variavel em um parametro de retorno
+
+            if( int.TryParse(textoDigitado, out numeroConvertido))
+            {
+                System.Console.WriteLine(numeroConvertido);
+            }
+            else
+            {
+                System.Console.WriteLine("A informação digitada não é um número");
+            }
+
+            textoDigitado = "123,45";
+            decimal.TryParse(textoDigitado, 
+            System.Globalization.NumberStyles.Currency, //moeda
+            System.Globalization.CultureInfo.CurrentCulture, //pt-BR
+            out decimal valorConvertido);
+            System.Console.WriteLine(valorConvertido);
         }
     }
 }
