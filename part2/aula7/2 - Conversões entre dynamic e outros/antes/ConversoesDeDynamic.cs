40aa203499f9666ab87d3e16a40c6c0c4335c43e
Emerson Vieira <emevieira@gmail.com>
2021-01-04 20:44:31 -0400

    Conhecendo conversoes dinamicas

--- part2/aula7/2 - Conversões entre dynamic e outros/antes/ConversoesDeDynamic.cs
+++ part2/aula7/2 - Conversões entre dynamic e outros/antes/ConversoesDeDynamic.cs
@@ -10,6 +10,26 @@ namespace certificacao_csharp_roteiro.antes
     {
         public void Executar()
         {
+            dynamic d1 = 7;
+            dynamic d2 = "String Qualquer";
+            dynamic d3 = DateTime.Now;
+            dynamic d4 = new List<int>() { 123, 456, 789};
+
+            System.Console.WriteLine(d1);
+            System.Console.WriteLine(d2);
+            System.Console.WriteLine(d3);
+            System.Console.WriteLine(d4);
+
+            int i = d1;
+            string str = d2;
+            DateTime dt = d3;
+            List<int> lista = d4;
+
+            System.Console.WriteLine(d1);
+            System.Console.WriteLine(d2);
+            System.Console.WriteLine(d3);
+            System.Console.WriteLine(d4);
+
         }
     }
 }
