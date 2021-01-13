da16a2631186a6f110018c704d38fd4acf907c70
Emerson Vieira <emevieira@gmail.com>
2021-01-12 21:25:14 -0400

    Propriedade somente de leitura

--- part3/Topico1.Propriedades e Acessadores/antes/Program.cs
+++ part3/Topico1.Propriedades e Acessadores/antes/Program.cs
@@ -10,7 +10,7 @@ namespace Topico1
     {
         static void Main(string[] args)
         {
-            Funcionario funcionario = Funcionario();
+            Funcionario funcionario = Funcionario(1000);
             // funcionario.salario = 1000;
             // System.Console.WriteLine(funcionario.salario);
 
@@ -25,22 +25,42 @@ namespace Topico1
 
     class Funcionario
     {
+
+        public Funcionario(decimal salario)
+        {
+                if(salario < 0)
+                {
+                    throw new ArgumentOutOfRangeException("salário não pode ser negativo.")
+                }
+            this.salario = salario;
+        }
+
         decimal salario;
 
-        public decimal Salario
+        public decimal Salario //encapsulamento do campo salario
         {
             get 
             {
                 return salario;
             }
-            set 
-            {
-                if(value < 0)
-                {
-                    throw new ArgumentOutOfRangeException("salário não pode ser negativo.")
-                }
-                salario = value;
-            }
+            // set 
+            // {
+            //     if(value < 0)
+            //     {
+            //         throw new ArgumentOutOfRangeException("salário não pode ser negativo.")
+            //     }
+            //     salario = value;
+            // }
         }
+
+        // private decimal salario;
+        // public decimal Salario
+        // {
+        //     get { return salario; }
+        //     set { salario = value; }
+        // }
+        
+        //propriedade auto implementada
+        // public decimal salario { get; set; }
     }
 }
