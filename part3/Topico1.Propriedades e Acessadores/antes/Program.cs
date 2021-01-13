26138d53080ac894520e8c11560bcf228e325568
Emerson Vieira <emevieira@gmail.com>
2021-01-12 20:53:28 -0400

    Encapsulamento - criado a classe Funcionario

--- part3/Topico1.Propriedades e Acessadores/antes/Program.cs
+++ part3/Topico1.Propriedades e Acessadores/antes/Program.cs
@@ -10,6 +10,37 @@ namespace Topico1
     {
         static void Main(string[] args)
         {
+            Funcionario funcionario = Funcionario();
+            // funcionario.salario = 1000;
+            // System.Console.WriteLine(funcionario.salario);
+
+            // funcionario.salario = -1200;
+            // System.Console.WriteLine(funcionario.salario);
+
+            //funcionario.Salario = -1200;
+            funcionario.Salario = 1200;
+            System.Console.WriteLine(funcionario.Salario);
+        }
+    }
+
+    class Funcionario
+    {
+        decimal salario;
+
+        public decimal Salario
+        {
+            get 
+            {
+                return salario;
+            }
+            set 
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("salário não pode ser negativo.")
+                }
+                salario = value;
+            }
         }
     }
 }
